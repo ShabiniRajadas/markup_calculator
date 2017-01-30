ec51323a5f0f626ee4cd45c47d6892a9d9a89992
Shabini Rajadas <shabiniraja90@gmail.com>
2017-01-30 18:00:24 +0400

    markup calculator

--- lib/markup_calculator.rb
+++ lib/markup_calculator.rb
@@ -1,4 +1,34 @@
 module MarkupCalculator
-	def self.calculate(a, b, c)
+	# calculate the total cost for the job
+	def self.calculate(base_price, labour_effort, markup)
+      	base_amount = base_markup(base_price)
+      	labour_price = labour_markup(base_amount, labour_effort)
+      	material_wise_price = material_markup(base_amount, markup)
+        total_amount =  base_amount + labour_price + material_wise_price
+        total_amount.round(2)
+	end
+
+	#calculate the basic markup imposed on all jobs
+	def self.base_markup(base_price)
+		(0.05 * base_price) + base_price
+	end
+
+	#calculate the markup for the labour effort needed for the job
+	def self.labour_markup(base_amount, labour_effort)
+		(0.012 * labour_effort) * base_amount
+	end
+
+	#calculate the markup based on the type of materials involved
+	def self.material_markup(base_amount, markup)
+		case markup
+		when 'pharmaceuticals', 'drugs'
+            0.075 * base_amount
+		when 'food'
+            0.13 * base_amount
+		when 'electronics'
+            0.02 * base_amount
+        else
+            0
+        end
 	end
 end
